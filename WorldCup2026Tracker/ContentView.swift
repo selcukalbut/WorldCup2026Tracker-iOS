68323df2f86d75796301189c29b276cb840621fa
Selcuk Albut <selcukalbut@me.com>
2026-06-03 15:38:22 +0300

    Initial Commit

---
+++ WorldCup2026Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorldCup2026Tracker
+//
+//  Created by Selcuk Albut on 3.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorldCup2026Tracker/WorldCup2026TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldCup2026TrackerApp.swift
+//  WorldCup2026Tracker
+//
+//  Created by Selcuk Albut on 3.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct WorldCup2026TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
